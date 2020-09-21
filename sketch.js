efa26de9ee4e4bc02f353092862b4c6f9300e2e0
Purvaansh <purvaanshranka@gmail.com>
2020-09-21 19:52:21 +0530

    bugs

--- sketch.js
+++ sketch.js
@@ -93,8 +93,10 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32 && bird.body.speed<1){
        slingshot.attach(bird.body);
+       bird.trajectory = [];
+       Matter.Body.setPosition(bird.body, {x:200, y:50});
     }
 }
 
